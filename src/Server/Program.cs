7d5e24a276ffa13a242753b53bd82f76055723ee
jplimw18 <jplima1853@gmail.com>
2025-07-17 17:11:11 -0300

    Estrutura inicial do projeto

---
+++ src/Server/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web browser server", Description = "Servidor de requisições de domínio", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WEB BROWSER SERVER V1");
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
